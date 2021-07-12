9709bc792e788fa7655019d7796199b81cf20051
“jianglang” <lang.jiang@ximalaya.com>
2021-07-12 11:53:06 +0800

    feat: 完善eslin校验初始化

--- src/03.basic.2.ts
+++ src/03.basic.2.ts
@@ -1,9 +1,8 @@
 // 类型断言
+const array1: string[] = [];
 
-const array1: string[] = []
+array1.push('2');
 
-array1.push('2')
-
-const arr2:number[] = [1, 2, 3]
-const res:number = arr2.find(num => num > 2) as number
-const res2:number = <number>arr2.find(num => num > 2)
+const arr2: number[] = [1, 2, 3];
+const res: number = arr2.find((num) => num > 2) as number;
+// const res2: number = <number>arr2.find((num) => num > 2);
